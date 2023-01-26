bc61042619f11d74761e7e63d1664f7b4af72a42
RAMYA5577 <mutyamramya0101@gmail.com>
2023-01-26 08:29:29 +0530

    changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,9 +3,9 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
    Product p = new Product();
-        System.out.println(p.product(5,6));
-        System.out.println(p.product(5,6,7));
-        System.out.println(p.product(6.2,7.5));
+        p.product(5,6);
+        p.product(5,6,7);
+        p.product(6.2,7.5);
 
     }
    static class Product{
